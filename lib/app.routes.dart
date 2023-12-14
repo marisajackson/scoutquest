5b8acf12bf512d2d1511e89b69d9794d58b094ff
MaRisa Jackson <mhljackson@gmail.com>
2023-12-13 18:43:09 -0600

    Splash Screen + App Icon

--- lib/app.routes.dart
+++ lib/app.routes.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:scoutquest/app/screens/clues/clues_screen.dart';
 import 'package:scoutquest/app/screens/quests/quests_screen.dart';
 import 'package:scoutquest/app/models/quest.dart';
+import 'package:scoutquest/app/screens/splash_screen.dart';
 
 const String initialRoute = '/';
 const String questsRoute = '/quests';
 const String cluesRoute = '/clues';
 
 final routes = {
-  initialRoute: (BuildContext context) => const QuestsScreen(),
+  initialRoute: (BuildContext context) => const SplashScreen(),
   questsRoute: (BuildContext context) => const QuestsScreen(),
   cluesRoute: (BuildContext context) {
     final Quest quest = ModalRoute.of(context)!.settings.arguments as Quest;
