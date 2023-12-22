b260b08cde2a5310d92ce68e0558b2cd7cfbe9d0
MaRisa Jackson <mhljackson@gmail.com>
2023-12-22 15:29:56 -0600

    Detailed empty state.

--- lib/app/screens/quests/quests_empty.dart
+++ lib/app/screens/quests/quests_empty.dart
@@ -12,23 +12,35 @@ class QuestsEmpty extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(
-            Icons.add_circle,
-            size: 100.0,
-            color: Colors.grey,
-          ),
-          const Text(
-            'No quests available',
-            style: TextStyle(
-                fontSize: 18.0, color: ScoutQuestColors.secondaryText),
-          ),
-          const SizedBox(height: 20.0),
-          FloatingActionButton.extended(
-              onPressed: onAddQuest, label: const Text('Add Quest')),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: 20.0), // Better padding for text
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.search,
+              size: 100.0,
+              color: Colors.grey,
+            ),
+            const Text(
+              'Welcome to Scout Quest! \n\n'
+              'Ready for adventure? \n'
+              'Visit scoutquest.co to find a quest near you and unlock the full potential of this app!',
+              textAlign: TextAlign
+                  .center, // Center-aligned text for better readability
+              style: TextStyle(
+                fontSize: 18.0,
+                color: ScoutQuestColors.secondaryText,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            FloatingActionButton.extended(
+              onPressed: onAddQuest,
+              label: const Text('Add Quest'),
+            ),
+          ],
+        ),
       ),
     );
   }
