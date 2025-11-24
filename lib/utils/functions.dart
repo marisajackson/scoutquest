7a7a6e906b6b97c6313a16f065838bb22b37de75
MaRisa Jackson <mhljackson@gmail.com>
2025-11-24 13:29:37 -0600

    Clean up

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -1,12 +1,9 @@
-import 'package:scoutquest/utils/logger.dart';
-
 import 'package:flutter/material.dart';
 
 Color colorFromHex(String? hex, {Color fallback = Colors.white}) {
   if (hex == null) return fallback;
   try {
     final h = hex.replaceAll('#', '');
-    Logger.log('Parsing hex color: $hex to $h');
     if (h.length == 6) return Color(int.parse('0xFF$h'));
     if (h.length == 8) return Color(int.parse('0x$h')); // already has alpha
   } catch (_) {}
